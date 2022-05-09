2c32fe9e03d8150884b0de5b350b19f0ebecfa75
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-05-08 23:35:45 -0500

    Add displayWarning() incase a repo has more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
 
 // http request to return a single repo and it's issues.
 var getRepoIssues = function(repo) {
@@ -10,8 +12,12 @@ var getRepoIssues = function(repo) {
     // request was successful
     if (response.ok) {
       response.json().then(function(data) {
-        // pass response data to dom function
         displayIssues(data);
+    
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     }
     else {
@@ -56,5 +62,17 @@ var displayIssues = function(issues) {
   }
 };
 
+var displayWarning = function(repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
 
 getRepoIssues("facebook/react");
